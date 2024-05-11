41e4519c419da591b4257590b4f002e6a5e0e446
Agustín Vazquez <avazquezcode@gmail.com>
2024-05-11 16:48:04 +0200

    Change clock native - make it in nano sec

--- internal/usecase/interpreter/native.go
+++ internal/usecase/interpreter/native.go
@@ -17,7 +17,7 @@ func (n FnClock) Arity() int {
 }
 
 func (n FnClock) Call(interpreter *Interpreter, arguments []interface{}) (interface{}, error) {
-	return time.Now().UnixMilli(), nil
+	return float64(time.Now().UnixNano()), nil
 }
 
 func (n FnSleep) Arity() int {
